dc61e8b0f0d0df6d728dbf695ad99fd3e1bde0e3
Szymon Nowacki <szymusnowacki@gmail.com>
2024-02-28 16:51:41 +0100

    Działa 1.20.1 odpalanie, mod i ovarall wszystko, kontynuacja możliwa

--- src/main/java/net/novaczuks/nova/Nova.java
+++ src/main/java/net/novaczuks/nova/Nova.java
@@ -52,19 +52,16 @@ public class Nova
 
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
 
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event)
     {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
